f49eecfe3246434543d3b4b0b6eb79db68bf7bc0
xQuantx <xquantxz@gmail.com>
2021-10-14 20:50:47 +0200

    x86/mbr: add video subroutines
    Signed-off-by: xQuantx <xquantxz@gmail.com>

--- arch/x86/boot/mbr.s
+++ arch/x86/boot/mbr.s
@@ -15,6 +15,7 @@ _start:
 	ljmp $0, $.L0
 .L0:
 	cli
+
 	xor %ax, %ax
 	mov %ax, %ds
 	mov %ax, %gs
@@ -24,13 +25,14 @@ _start:
 
 	mov $__mbr_start, %bp
 	mov %bp, %sp
+
 	sti
 
 	inc %al
 	push %ax
 	mov $2, %ah
 	mov $0x7E00, %bx
-	mov $0, %ch
+	xor %ch, %ch
 	mov $2, %cl
 	int $0x13
 	jc load_error
@@ -40,7 +42,32 @@ _start:
 	jmp _loader
 
 load_error:
+	mov $4, %bl
+	call bios_set_bgcolor
+	mov $.Lload_error_message, %si
+	call bios_print
 	jmp .
+
+bios_print:
+	pusha
+	mov $0x0E, %ah
+.Lloop_bios_print:
+	lodsb
+	int $0x10
+	cmpb $0, (%si)
+	jne .Lloop_bios_print
+	popa
+	ret
+
+bios_set_bgcolor:
+	pusha
+	mov $0x0B, %ah
+	xor %bh, %bh
+	int $0x10
+	popa
+	ret
+
+.Lload_error_message: .ascii "Couldn't load the second stage loader into memory"
 
 .org 0x1FE
 .word BOOT_SIGNATURE
