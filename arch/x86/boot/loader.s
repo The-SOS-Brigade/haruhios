77ed64004094d127a217cd1c071a3df857d4f7ac
xQuantx <xquantxz@gmail.com>
2021-10-14 20:54:14 +0200

    x86/boot: prepare for pm_entry
    Signed-off-by: xQuantx <xquantxz@gmail.com>

--- arch/x86/boot/loader.s
+++ arch/x86/boot/loader.s
@@ -7,6 +7,29 @@
 .global _loader
 
 _loader:
-	jmp .
+	cli
+	
+	inb $0x92, %al
+	or $2, %al
+	outb %al, $0x92
 
+	lgdt gdt_desc
+	
+	mov %cr0, %eax
+	or $1, %eax
+	mov %eax, %cr0
+
+	ljmp $code_seg, $pm_entry
+
+.code32
+pm_entry:
+	mov $data_seg, %eax
+	mov %eax, %ds
+	mov %eax, %ss
+	mov %eax, %es
+	mov %eax, %fs
+	mov %eax, %gs
+
+_exit:
+	jmp _exit
 
---
+++ arch/x86/boot/segment.s
@@ -0,0 +1,31 @@
+
+.global __gdt_start, __gdt_end
+.global code_seg, data_seg
+
+__gdt_start:
+.Lnull_gdt:
+	.quad 0
+.Lcode_gdt:
+	.word 0xFFFF
+	.word 0x0000
+	.byte 0b00000000
+	.byte 0b10011110
+	.byte 0b11001111
+	.byte 0b00000000
+.Ldata_gdt:
+	.word 0xFFFF
+	.word 0x0000
+	.byte 0b00000000
+	.byte 0b10010010
+	.byte 0b11001111
+	.byte 0b00000000
+__gdt_end:
+
+gdt_desc:
+	.word __gdt_end - __gdt_start - 1
+	.word __gdt_start
+	.word 0
+
+code_seg = .Lcode_gdt - __gdt_start
+data_seg = .Ldata_gdt - __gdt_start
+
