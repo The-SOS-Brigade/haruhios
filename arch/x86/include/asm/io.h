1de814f3f64a67e1cfd08279a2cbc09e3246ffa8
xQuantx <xquantxz@gmail.com>
2021-11-27 11:09:15 +0100

    x86/io: define io_wait
    Signed-off-by: xQuantx <xquantxz@gmail.com>

--- arch/x86/include/asm/io.h
+++ arch/x86/include/asm/io.h
@@ -6,25 +6,30 @@
 static inline u8 inb(u16 port)
 {
 	u8 data;
-	__asm__ __volatile__("inb %w1, %b0" : "=a" (data) : "d" (port));
+	__asm__ __volatile__("inb %w1, %b0" : "=a" (data) : "dN" (port));
 	return data;
 }
 
 static inline void outb(u16 port, u8 data)
 {
-	__asm__ __volatile__("outb %b0, %w1" :: "a" (data), "d" (port));
+	__asm__ __volatile__("outb %b0, %w1" :: "a" (data), "dN" (port));
 }
 
 static inline u16 inw(u16 port)
 {
 	u16 data;
-	__asm__ __volatile__("inw %w1, %w0" : "=a" (data) : "d" (port));
+	__asm__ __volatile__("inw %w1, %w0" : "=a" (data) : "dN" (port));
 	return data;
 }
 
 static inline void outw(u16 port, u16 data)
 {
-	__asm__ __volatile__("outw %w0, %w1" :: "a" (data), "d" (port));
+	__asm__ __volatile__("outw %w0, %w1" :: "a" (data), "dN" (port));
+}
+
+static inline void io_wait(void)
+{
+	__asm__ __volatile__("outb %0, %1" :: "a"(0), "dN"(0x80));
 }
 
 #endif
